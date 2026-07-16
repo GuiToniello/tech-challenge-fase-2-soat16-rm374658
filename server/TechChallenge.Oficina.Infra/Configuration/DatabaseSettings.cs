cf4b8d514c6e624947fb829a3b3ef73d747a7911
Guilherme Toniello <guilherme.toniello@ndd.tech>
2026-07-16 18:28:06 -0300

    refactor: remover projeto nao usado

--- server/TechChallenge.Oficina.Infra/Configuration/DatabaseSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace TechChallenge.Oficina.Infra.Configuration;
-
-public sealed class DatabaseSettings
-{
-    public const string SectionName = "DatabaseSettings";
-
-    public string ConnectionString { get; init; } = string.Empty;
-}
